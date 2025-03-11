95f8c4df4865b61193a9469f9e2aa6718b571e17
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-03-11 13:10:01 +0300

    done with the signup and login page

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,10 +1,41 @@
+"use client"
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 
 const SignUp = () => {
   return (
-    <div>
-    <h1>Sign up</h1>
-    </div>
+    <div className='grid text-[#131524] grid-cols-3 w-full h-screen items-center'>
+          <div className='flex col-span-1 items-center flex-col w-full bg-[#FA7275] h-full text-white'>
+            <div className='flex mt-[50px] w-[300px] items-center'>
+              <Image src='/pinterest.png' alt='logo' width={40} height={40} />
+              <h1 className='text-xl ml-3'>My<span className='text-gray-200'>S</span>tore</h1>
+            </div>
+            <div className='flex w-[300px] mt-5 flex-col '>
+              <h1 className='flex text-2xl font-bold '>Manage your files</h1>
+              <h1 className='flex text-2xl font-bold '>The best way</h1>
+            </div>
+            <div className='mt-4'>
+              <p className='flex w-[300px] text-sm '>Awesome, we've created the perfect place for you to store all your documents.</p>
+            </div>
+            <div className='mt-5'>
+              <Image src='/files.png' alt='files logo' width={250} height={250}/>
+            </div>
+          </div>
+          <div className='flex items-center justify-center col-span-2 flex-col h-full'>
+            <div className='flex w-[400px]'>
+              <h1 className='font-bold text-3xl text-[#131524] '>Create Account</h1>
+            </div>
+            <form className='w-[400px] mt-7'>
+            <input type='text' name='name' placeholder='Enter your full name ... ' className='text-[10px] p-4 w-[400px] rounded-md outline-none shadow-sm' />
+              <input type='text' name='email' placeholder='Enter your email ... ' className='text-[10px] mt-7 p-4 w-[400px] rounded-md outline-none shadow-sm' />
+              <button className='flex cursor-pointer text-[10px] p-4 rounded-full w-[400px] items-center justify-center text-white bg-[#FA7275] mt-6 shadow-sm'>Create Account</button>
+            </form>
+            <div className='mt-4'><p className='text-[10px]'>Already have an account? <Link className='text-[#FA7275]' href='/'>Sign in</Link></p></div>
+          </div>
+          
+        </div>
   )
 }
 
